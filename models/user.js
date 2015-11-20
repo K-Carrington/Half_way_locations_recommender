658093a69829b13d96f7a07275b76c5ce068c102
nickhendren <nhendren7@gmail.com>
2015-11-20 15:15:33 -0800

    working on user model and server.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,26 @@
-//user model
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = new mongoose.Schema({
+  local: {
+    first_name: String,
+    last_name: String,
+    email: {type: String, required: true, unique: true},
+    password: String
+  },
+  facebook: {
+    id: String,
+    name: String,
+    token: String,
+    email: String
+  }
+});
+
+userSchema.methods.generateHash = function(password){
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+}
+
+userSchema.methods.validPassword = function(password){
+  var user = this;
+  return bcrypt.compareSync(password, user.local.password);
+}
--- server.js
+++ server.js
@@ -1,2 +1,26 @@
-var express = require('express'),
-    app     = express
+var express     = require('express');
+var app         = express();
+var logger      = require('morgan');
+var mongoose    = require('mongoose');
+var bodyParser  = require('body-parser');
+var port        = process.env.PORT || 3000;
+
+//connect to database
+var db = 'halfway_meet';
+mongoose.connect('mongodb://localhost/' + db, function(err){
+  if(err) return console.log('Cannot connect to ' + db + ' database.');
+  console.log('mongodb connected to ' + db + ' database.');
+});
+
+//middleware
+app.use(logger('dev'));
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+
+//static index
+app.use(express.static('public'));
+
+app.listen(port, function(){
+  console.log('Server running on ' + port);
+});
