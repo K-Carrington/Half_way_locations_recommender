215c9069d667b774bd7cbcef89776007a9ff9bd0
nickhendren <nhendren7@gmail.com>
2015-11-23 19:24:49 -0800

    cleaned up the code for yelp.js

--- config/yelp.js
+++ config/yelp.js
@@ -8,27 +8,4 @@ var yelp = new Yelp({
   token_secret: 'vH7CfxS2POsC10T5GyrqnjNhLuQ',
 });
 
-//See http://www.yelp.com/developers/documentation/v2/search_api
-
-
-//window.onload = function() {
-//   $.ajax({
-//     url: 'views/yelp',
-//     method: 'GET',
-//     success: function(data){
-//         yelp.search({ term: 'bar', location: '1520 2nd Street, Santa Monica, CA', limit: 10})
-//           .then(function (data) {
-//             for (var i = 0; i < data.businesses.length; i++){
-//               console.log(data.businesses[i].name);
-//               console.log(data.businesses[i].url);
-//               console.log(data.businesses[i].location);
-//             }
-//           })
-//         .catch(function (err) {
-//           console.error(err);
-//         });
-//     }
-//   })
-// }
-
 module.exports = yelp;
